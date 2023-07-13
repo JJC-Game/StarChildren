8a9cb1f39259200af98ef2f2783676706a6c74e7
YoshizawaFubuki <fubukichi2000@gmail.com>
2023-07-13 13:24:18 +0900

    PauseManager追加

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public Canvas PauseCanvas;
+    public Canvas MainGame;
+    
+    void Start()
+    {
+        PauseCanvas.enabled = false;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void Pause()
+    {
+        GameManager.Instance.mainGame = false;
+        MainGame.enabled = false;
+        PauseCanvas.enabled = true;
+        Time.timeScale = 0f; // ゲーム時間を停止します
+
+    }
+
+}
