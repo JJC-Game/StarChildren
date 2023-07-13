a72717b7bf4d0661d582b9a6fd5202e119469453
Yoshiokasama <yoshioka10180422@gmail.com>
2023-07-13 15:16:28 +0900

    プレイヤーの高さを表示するスクリプトをつくった

--- Assets/Scripts/HeightTracker.cs
+++ Assets/Scripts/HeightTracker.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HeightTracker : MonoBehaviour
 {
-    public Text heightText; // 高さを表示するテキスト
+    public TextMeshProUGUI heightText; // 高さを表示するテキスト
 
     private void Update()
     {
         // プレイヤーの現在の高さを取得
-        float playerHeight = transform.position.y;
+        float playerHeight = transform.position.y+7;
 
         // 高さをテキストに表示
-        heightText.text = "Height: " + playerHeight.ToString("F1") + "m";
+        heightText.text = "Height:" + Mathf.RoundToInt(playerHeight).ToString()  + "m";
     }
 }
+
