9f8354790ee062cbd9d00ffd4d5c76bccafafce0
Yoshiokasama <yoshioka10180422@gmail.com>
2023-07-13 12:40:49 +0900

    ぼでいをプレハブに入れた

---
+++ Assets/Scripts/HeightTracker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeightTracker : MonoBehaviour
+{
+    public Text heightText; // 高さを表示するテキスト
+
+    private void Update()
+    {
+        // プレイヤーの現在の高さを取得
+        float playerHeight = transform.position.y;
+
+        // 高さをテキストに表示
+        heightText.text = "Height: " + playerHeight.ToString("F1") + "m";
+    }
+}
