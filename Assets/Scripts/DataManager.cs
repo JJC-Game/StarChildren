75bcd788a0c0af4662b00cc4b03c8d4e2aa28d12
YoshizawaFubuki <fubukichi2000@gmail.com>
2023-07-25 14:33:21 +0900

    DataManagerに使用できる変数を追加(float,bool)

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class DataManager : Singleton<DataManager>
 {
-    // 変数の保存
+    // 変数の保存int型
     public void SaveInt(string key, int value)
     {
         PlayerPrefs.SetInt(key, value);
         PlayerPrefs.Save();
     }
 
-    // 保存した変数の呼び出し
+    // 変数の保存float型
+    public void SaveFloat(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
+    }
+
+    // 変数の保存bool型（1がtrue、0がfalse）
+    public void SaveBool(string key, bool value)
+    {
+        PlayerPrefs.SetInt(key, value ? 1 : 0); // 1 for true, 0 for false
+        PlayerPrefs.Save();
+    }
+
+    // 保存した変数の呼び出しint型
     public int LoadInt(string key)
     {
         return PlayerPrefs.GetInt(key);
     }
+
+    // 保存した変数の呼び出しfloat型
+    public float LoadFloat(string key)
+    {
+        return PlayerPrefs.GetFloat(key);
+    }
+
+    // 保存した変数の呼び出しbool型
+    public bool LoadBool(string key)
+    {
+        return PlayerPrefs.GetInt(key) == 1;
+    }
+
 
     // 全変数のリセット
     public void ResetAllVariables()
