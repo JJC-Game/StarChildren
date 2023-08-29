d57fe458789a10c91faa7db4f8a625a4ff99c4f1
haniudahiroto <hara.jiburarutal@gmail.com>
2023-08-29 17:38:18 +0900

    エフェクトの編集

--- Assets/Scripts/EffectManager.cs
+++ Assets/Scripts/EffectManager.cs
@@ -4,19 +4,25 @@ using UnityEngine.UI;
 
 public class EffectManager : Singleton<EffectManager>
 {
-    public GameObject[] effectPrefabs; // エフェクトのプレハブを配列で管理
+    // 2Dパーティクルエフェクトのプレハブ配列
+    public GameObject[] particleEffectPrefabs;
+    public float deleteTime = 1.0f;
 
     // エフェクトを再生するメソッド
-    public void PlayEffect(int effectIndex, Vector3 position)
+    public void PlayEffect(int index, Transform targetTransform)
     {
-        if (effectIndex < 0 || effectIndex >= effectPrefabs.Length)
+        if (index >= 0 && index < particleEffectPrefabs.Length)
         {
-            Debug.LogWarning("Invalid effect index: " + effectIndex);
-            return;
-        }
+            GameObject effectPrefab = particleEffectPrefabs[index];
 
-        Instantiate(effectPrefabs[effectIndex], position, Quaternion.identity);
+            if (effectPrefab != null && targetTransform != null)
+            {
+                // ターゲットの位置にエフェクトを再生
+                GameObject effectInstance = Instantiate(effectPrefab, targetTransform.position, Quaternion.identity);
+
+                // エフェクトの再生が終わったら一定時間後に破棄
+                Destroy(effectInstance, deleteTime);
+            }
+        }
     }
 }
-
-
