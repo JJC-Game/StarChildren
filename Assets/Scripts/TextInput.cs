2b2fe34b0a20775ad2cf1c7b7f60b16bb2b99c12
YoshizawaFubuki <fubukichi2000@gmail.com>
2023-09-05 11:54:11 +0900

    名前入力再度修正

--- Assets/Scripts/TextInput.cs
+++ Assets/Scripts/TextInput.cs
@@ -38,6 +38,8 @@ public class TextInput : MonoBehaviour
 
             // キーボードを解放
             keyboard = null;
+            MainGameCanvas.gameObject.SetActive(true);
+            Back.gameObject.SetActive(false);
         }
     }
 
