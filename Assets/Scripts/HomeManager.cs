3e0c575c1365fb91ebeb8e9a7c572957507b65e1
YoshizawaFubuki <fubukichi2000@gmail.com>
2023-09-06 15:28:22 +0900

    ホームのキャラの見た目も変わるように変更

--- Assets/Scripts/HomeManager.cs
+++ Assets/Scripts/HomeManager.cs
@@ -14,12 +14,66 @@ public class HomeManager : MonoBehaviour
 
     private int currentButtonIndex = 0; // 現在のボタンのインデックス
 
+    public GameObject PlayerSprite; //キャラクターの見た目
+    public Sprite[] EvolveSprite; //進化後のキャラクターの見た目
+
     void Start()
     {
         StageSelectCanvas.gameObject.SetActive(false);
         StageSelectChange = false;
         StageSelectButton[1].gameObject.SetActive(false);
         StageSelectButton[2].gameObject.SetActive(false);
+
+        if (DataManager.Instance.LoadBool("E1F") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[0];
+        }
+        else if (DataManager.Instance.LoadBool("E1O") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[1];
+        }
+        else if (DataManager.Instance.LoadBool("E2FF") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[2];
+        }
+        else if (DataManager.Instance.LoadBool("E2OO") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[3];
+        }
+        else if (DataManager.Instance.LoadBool("E2FO") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[4];
+        }
+        else if (DataManager.Instance.LoadBool("E3FFF") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[5];
+        }
+        else if (DataManager.Instance.LoadBool("E3OOO") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[6];
+        }
+        else if (DataManager.Instance.LoadBool("E3FFO") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[7];
+        }
+        else if (DataManager.Instance.LoadBool("E3FOO") == true)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[8];
+        }
+        else if (DataManager.Instance.LoadBool("E1") == false)
+        {
+            SpriteRenderer targetSpriteRenderer = PlayerSprite.GetComponent<SpriteRenderer>();
+            targetSpriteRenderer.sprite = EvolveSprite[9];
+        }
     }
 
     void Update()
