fb0c8b82b81ba8398c2d5772d2b003a7571c11b1
YoshizawaFubuki <fubukichi2000@gmail.com>
2023-09-11 15:09:22 +0900

    チュートリアル追加とホームと育成の画像差し替え

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class TutorialManager : MonoBehaviour
 {
     public bool T1;
+    public bool T2;
+    public bool T3;
+    public bool T4;
 
+    public GameObject Canvas1;
+    public GameObject Canvas2;
+    public GameObject Canvas3;
+    public GameObject Canvas4;
+    public GameObject Canvas5;
 
     void Start()
     {
