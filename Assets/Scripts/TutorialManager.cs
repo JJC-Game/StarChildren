54e679d7ad52bee1453abe6511b926eb2502e763
YoshizawaFubuki <fubukichi2000@gmail.com>
2023-09-11 14:02:05 +0900

    ステージ２編集とチュートリアル作成途中

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public bool T1;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
