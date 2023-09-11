663078e48697a0c4ad40d2967e29fdaa5e2aa26e
Yoshiki <yosikidesu8@gmail.com>
2023-09-11 15:18:46 +0900

    TitleのSEを変更しました

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -20,7 +20,7 @@ public class TitleManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && Home)
         {
-            SoundManager.Instance.PlaySE_Sys(6);
+            SoundManager.Instance.PlaySE_Sys(9);
             FadeManager.Instance.LoadScene("MHomeScene", 1);
             //SceneManager.LoadScene(1);
             DataManager.Instance.SaveBool("Home",false);
